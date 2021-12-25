1e721b6f2e1ff98ae9bfae43365aa5ddd3f49a64
Be445 <96068915+Be445@users.noreply.github.com>
2021-12-25 09:50:33 +0700

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,6 +1,4 @@
-sudo su
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccmine...
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
